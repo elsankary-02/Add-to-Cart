21f53287db132c9b1e43e805f3fd653d5678cc6c
elsankary-02 <githup800900@gmail.com>
2025-01-19 18:19:27 +0200

    add image to use for app

---
+++ lib/core/components/image_manger.dart
@@ -0,0 +1,10 @@
+class ImageManger {
+  static const kFruitSoup = 'assets/images/Fruit_soup.png';
+  static const kIceCream = 'assets/images/ice_cream.png';
+  static const kLentilsSoup = 'assets/images/lentils_soup.png';
+  static const kMeat = 'assets/images/meat.png';
+  static const kPasta = 'assets/images/pasta.png';
+  static const kPizza = 'assets/images/pizza.png';
+  static const kSolves = 'assets/images/solves.png';
+  static const kVegetableSalad = 'assets/images/Vegetable_salad.png';
+}
